c2646514b2dd86a39bab58588027d8b208bcfccc
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-06 20:25:49 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 
 class Square:
-    
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     def area_of_my_square(self):
-        """ Calculates the area of the square """
-        return self.width * self.width
+        """ Area of the square """
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
-        """ Calculates the perimeter of the square """
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "Square [Width: {}, Height: {}]".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
-    print("Area:", s.area_of_my_square())
-    print("Perimeter:", s.perimeter_of_my_square())
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
